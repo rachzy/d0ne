770149d34adb9f8e17ec42031788866a273cfe62
Henrique <henriqueferreira.fox@gmail.com>
2023-11-29 08:25:44 -0300

    fix loader overflow on buttons

--- src/components/SignOutIcon/index.tsx
+++ src/components/SignOutIcon/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './SignOutIcon.module.css';
 
 import { FaRightFromBracket } from "react-icons/fa6";
+import Loader from '../Loader';
 
 interface IProps {
     onClick: () => void;
 }
 
 const SignOutIcon: React.FC<IProps> = ({onClick}) => {
-  return (
-    <FaRightFromBracket className={classes.icon} onClick={onClick} />
+  const [clicked, setClicked] = useState(false);
+
+  async function handleIconClick() {
+    setClicked(true);
+    await onClick();
+    setClicked(false);
+  }
+
+  return(
+    <div className={classes.icon_box}>
+      {clicked ? <Loader /> : <FaRightFromBracket className={classes.icon} onClick={handleIconClick} title={"Sign out"} />}
+    </div>
   )
 }
 
